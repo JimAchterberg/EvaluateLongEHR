9b1d60fa6f0b2025063e3d59850b00607d0225f0
JimAchterberg <jimachterberg1@gmail.com>
2024-02-14 14:26:50 +0100

    removed unnecesseary scripts

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+#numerically encode categoricals, one hot if necessary, separate static and sequential data
 def prepr(real_df,syn_df):
         n_real = real_df.subject_id.nunique()
         #pool samples for encoding
@@ -46,22 +47,6 @@ def df_to_3d(df,cols,subject_idx='subject_id',timestep_idx='seq_num',padding='-1
     return seq
 
 
-#one hot encode non-binary categoricals
-def one_hot_encoding(data,columns,column_sizes=None):
-    for j,i in enumerate(columns):
-        dummies = pd.get_dummies(data[i])
-        dummies = dummies.reset_index(drop=True)
-        if column_sizes[j]>dummies.shape[1]:
-            zeros = pd.DataFrame(np.zeros(shape=(data.shape[0],column_sizes[j]-dummies.shape[1])))
-            dummies = pd.concat([dummies,zeros],axis=1)
-        column_names = []
-        for n in range(dummies.shape[1]):
-            column_names.append(str(i)+'_'+str(n))
-        dummies.columns = column_names
-        data = data.drop(i,axis=1)
-        data = data.reset_index(drop=True)
-        data = pd.concat([data,dummies],axis=1)
-    return data
 
 class Scaler():
     def __init__(self,method='zero-one'):
@@ -78,46 +63,13 @@ class Scaler():
         if self.method=='zero-one':
             return x*(self.max-self.min) + self.min
         
-
-
-
-#normalizing function for pandas dataframe
-def normalize(x):
-    return (x-x.mean())/(x.std())
-
-
-
-#zero one scaling function for pandas dataframe
-def zero_one_scale(x):
-    return (x-x.min())/(x.max()-x.min())
     
 
 def train_split(X,y,stratify=None,train_size=.7):
     #create train test split stratified on syn/real labels
     return train_test_split(X,y,stratify=stratify,train_size=train_size)
 
-#turns 2d dataframe into list of lists of variable length sequences
-def get_sequences(df,column,subject_idx='subject_id',return_subject_idx=False):
-    seq = []
-    sbj = []
-    for i in df[subject_idx].unique():
-        seq.append(df[df[subject_idx]==i][column].to_list())
-        sbj.append(i)
-    if return_subject_idx:
-        return seq,sbj
-    else:
-        return seq
 
-#one hot encode 3d categorical array, while ensuring correct cardinality by concatting zeros
-def one_hot_3d(data,cardinality):
-    n,t,k = data.shape
-    dummies = pd.get_dummies(data.flatten()) 
-    if dummies.shape[1]<cardinality:
-        dummies = dummies.reset_index()
-        zeros = pd.DataFrame(np.zeros((dummies.shape[0],cardinality-dummies.shape[1])))
-        dummies = pd.concat([dummies,zeros],axis=1)
-    dummies = dummies.to_numpy().reshape((n,t,dummies.shape[1]))
-    return dummies
 
 def trajectory_input_output(x,max_t):
     static = x[0]
